3154796aa3f3422314909c812bd23e2e37cc6d3d
Carlos Kelley <carloskelley@protonmail.com>
2022-05-31 13:02:52 -0500

    tasks up

--- server/server.js
+++ server/server.js
@@ -2,12 +2,15 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const tasks = require('./modules/tasks');
 //uses
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use('/tasks', tasks);
 //globals
 const port = 5001;
 //server
 app.listen(port, ()=>{
     console.log ('server up:', port);
 })
+
