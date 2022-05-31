6727e5bc3be13367246c67b668ec5a13cdd5e68c
Carlos Kelley <carloskelley@protonmail.com>
2022-05-31 13:37:00 -0500

    get ajax working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,19 @@ $( document ).ready( onReady );
 
 function onReady(){
     console.log('JQ');
+    getTasks();
+}
+
+function getTasks(){
+    console.log( 'in getTasks' );
+    // AJAX GET call to /tasks
+    $.ajax({
+        method: 'GET',
+        url: '/tasks'
+    }).then( function( response ){
+        console.log(response);
+    }).catch( function( err ){
+        console.log( err );
+        alert( 'error getting tasks' );
+    })
 }
